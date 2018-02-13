418c16bafe8dbe1c0e061bcd880578bcf8ef9985
Ameilorate <ameilorate2@gmail.com>
2018-02-13 13:58:07 -0800

    Fix typo in start.sh

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ CONFIG_FILE=${ANM3_CONFIG_FILE:-"/etc/anm3rc.awk"}
 if [ -r ./anm3rc.awk ]; then
 	CONFIG_FILE="./anm3rc.awk"
 elif [ -r $HOME/.config/anm3/config.awk ]; then
-	CONFIG_FILE="$HOME/.config/amn3/config.awk"
+	CONFIG_FILE="$HOME/.config/anm3/config.awk"
 elif [ -r $HOME/.anm3rc.awk ]; then
 	CONFIG_FILE="$HOME/.anm3rc.awk"
 fi
