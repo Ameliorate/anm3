00f8a92a36b7213c7959a9821a52c610ca4a9920
Ameilorate <ameilorate2@gmail.com>
2018-02-15 19:57:08 -0800

    Rename start.sh to anm3.sh

---
+++ anm3.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+CONFIG_FILE=${ANM3_CONFIG_FILE:-"/etc/anm3rc.awk"}
+
+if [ -r ./anm3rc.awk ]; then
+	CONFIG_FILE="./anm3rc.awk"
+elif [ -r $HOME/.config/anm3/config.awk ]; then
+	CONFIG_FILE="$HOME/.config/anm3/config.awk"
+elif [ -r $HOME/.anm3rc.awk ]; then
+	CONFIG_FILE="$HOME/.anm3rc.awk"
+fi
+
+cat $CONFIG_FILE | gawk -f configcheck.awk
+AWKPATH=$AWKPATH:/usr/share/anm3:. eval "nmfd | gawk -f $CONFIG_FILE"
